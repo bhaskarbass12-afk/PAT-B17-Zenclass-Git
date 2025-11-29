70191fe86a47deafbda86419b509d43a84a3dd18
alagarsamy <balagars@amazon.com>
2025-11-29 16:04:31 +0530

    comments are updated

--- Input_variable_Example.py
+++ Input_variable_Example.py
@@ -1,7 +1,7 @@
-name=input()
-score= int(input())
+name=input() #input will only cast string
+score= int(input()) #if we put int(input()) we can caste integer.
 department=input()
 print("My Name is:", name)
 print("My Score is:", score/10, "/10")
-print("My department is:", department)
+print("My department is:", department) 
 
